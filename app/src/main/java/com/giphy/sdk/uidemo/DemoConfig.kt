8f3a061faecaa28d328145ce9497773c02a730de
Cristian Holdunu <cristian.holdunu@gmail.com>
2020-02-13 21:11:47 +0200

    update demo app

---
+++ app/src/main/java/com/giphy/sdk/uidemo/DemoConfig.kt
@@ -0,0 +1,11 @@
+package com.giphy.sdk.uidemo
+
+import com.giphy.sdk.core.models.enums.MediaType
+import com.giphy.sdk.ui.views.GiphyGridView
+
+object DemoConfig {
+    var spanCount = 2
+    var cellPadding = 0
+    var mediaType = MediaType.gif
+    var direction = GiphyGridView.VERTICAL
+}
