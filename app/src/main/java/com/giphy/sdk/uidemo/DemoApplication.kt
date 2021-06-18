16cd1d3a62922ba804bdf372753b9792b3d0819e
Alexander Lonsky <alexander.lonsky@gmail.com>
2021-06-18 14:21:34 +0300

    - update to SDK 2.1.1

--- app/src/main/java/com/giphy/sdk/uidemo/DemoApplication.kt
+++ app/src/main/java/com/giphy/sdk/uidemo/DemoApplication.kt
@@ -4,15 +4,4 @@ import android.app.Application
 import com.squareup.leakcanary.LeakCanary
 
 class DemoApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.FLAVOR == "debug") {
-            if (LeakCanary.isInAnalyzerProcess(this)) {
-                // This process is dedicated to LeakCanary for heap analysis.
-                // You should not init your app in this process.
-                return
-            }
-            LeakCanary.install(this)
-        }
-    }
 }
